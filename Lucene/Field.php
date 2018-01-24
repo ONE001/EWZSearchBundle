a816f45ffe56b107582c2907e04853b789b3d054
Dominik Businger <github@azine-it.ch>
2018-01-24 09:48:00 +0100

    Fix inheritance issue

--- Lucene/Field.php
+++ Lucene/Field.php
@@ -6,6 +6,83 @@ use Zend\Search\Lucene\Document\Field as ZendField;
 
 class Field extends ZendField
 {
+
+
+    /**
+     * Constructs a String-valued Field that is not tokenized, but is indexed
+     * and stored.  Useful for non-text fields, e.g. date or url.
+     *
+     * @param string $name
+     * @param string $value
+     * @param string $encoding
+     * @return \Zend\Search\Lucene\Document\Field
+     */
+    public static function keyword($name, $value, $encoding = 'UTF-8')
+    {
+        return new self($name, $value, $encoding, true, true, false);
+    }
+
+
+    /**
+     * Constructs a String-valued Field that is not tokenized nor indexed,
+     * but is stored in the index, for return with hits.
+     *
+     * @param string $name
+     * @param string $value
+     * @param string $encoding
+     * @return \Zend\Search\Lucene\Document\Field
+     */
+    public static function unIndexed($name, $value, $encoding = 'UTF-8')
+    {
+        return new self($name, $value, $encoding, true, false, false);
+    }
+
+
+    /**
+     * Constructs a Binary String valued Field that is not tokenized nor indexed,
+     * but is stored in the index, for return with hits.
+     *
+     * @param string $name
+     * @param string $value
+     * @param string $encoding
+     * @return \Zend\Search\Lucene\Document\Field
+     */
+    public static function binary($name, $value)
+    {
+        return new self($name, $value, '', true, false, false, true);
+    }
+
+    /**
+     * Constructs a String-valued Field that is tokenized and indexed,
+     * and is stored in the index, for return with hits.  Useful for short text
+     * fields, like "title" or "subject". Term vector will not be stored for this field.
+     *
+     * @param string $name
+     * @param string $value
+     * @param string $encoding
+     * @return \Zend\Search\Lucene\Document\Field
+     */
+    public static function text($name, $value, $encoding = 'UTF-8')
+    {
+        return new self($name, $value, $encoding, true, true, true);
+    }
+
+
+    /**
+     * Constructs a String-valued Field that is tokenized and indexed,
+     * but that is not stored in the index.
+     *
+     * @param string $name
+     * @param string $value
+     * @param string $encoding
+     * @return \Zend\Search\Lucene\Document\Field
+     */
+    public static function unStored($name, $value, $encoding = 'UTF-8')
+    {
+        return new self($name, $value, $encoding, false, true, true);
+    }
+
+
     /*
      * convience function to find the way the field was created
      * instead of having to check the is* individually
